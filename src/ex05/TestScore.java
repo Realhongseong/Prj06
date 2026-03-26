3cc95b5608fa3b042a0df771e8a305a5b989a2f8
Realhongseong <xowl179@gmail.com>
2026-03-26 15:42:41 +0900

    ex05 added

---
+++ src/ex05/TestScore.java
@@ -0,0 +1,31 @@
+package ex05;
+
+/*
+num,name,kor,eng,mat
+번호,이름,국어,영어,수학
+1,사나,70,80,90
+2,모모,0,70,100
+3,쯔위,70,90,90
+4,정연,80,90,70
+5,원영,90,90,89
+6,유진,100,100,100
+*/
+
+// 입력:번호,이름,국어,영어,수학
+// 출력:번호 이름 국어 영어 수학 총점 평균 등급
+// 모든 Field는 private
+// 생성자, getter/setter, toString() 만들어서 작업
+
+
+class Score{
+	private
+}
+
+public class TestScore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
